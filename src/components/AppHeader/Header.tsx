aab8150e294130fd93c67a6994774f827c73d972
KekichSergich <nikitakild.0989@gmail.com>
2026-01-12 23:48:39 +0100

    first commit

---
+++ src/components/AppHeader/Header.tsx
@@ -0,0 +1,10 @@
+import '../styles/header.css';
+import '../styles/App.css';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>TODO LIST</h1>
+        </header>
+    )
+}
